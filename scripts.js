b45711edf5f86391d7b80676af4a1163b950db33
Мария <87368091+MariaKorkina@users.noreply.github.com>
2021-09-14 20:39:50 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-//1 - получаем данных из json с помощью fetch-запроса
+// получаем данных из json с помощью fetch-запроса
 function getData() {
   return fetch('data.json')
     .then(response => {
@@ -14,9 +14,10 @@ function getData() {
     });
 }
 
-//функция, разбивающая массив на подмассивы
+//доп цель - функция, осуществляющая постраничный вывод данных из предоставленного JSON (10 строк на страницу)
 function splitArray(arr, arraySize = 10) {
   const subarray = [],
+        // с помощью метода Math.ceil округляем до ближайшего большего целого числа
         subarrayAmount = Math.ceil(arr.length / arraySize);
   
   for (let i = 0; i < subarrayAmount; i++) {
@@ -25,7 +26,8 @@ function splitArray(arr, arraySize = 10) {
   
   return subarray;
 }
-//функция отрисовывает пагинацию и вызывает колбэком функицю отрисовки страницы, выбранной в пагинации 
+
+// отрисовываем пагинацию и вызываем колбэком функцию отрисовки  выбранной в пагинации страницы
 function renderPagination(jsonData) {
   const data = localStorage.getItem('jsonData') ? JSON.parse( localStorage.getItem('jsonData') ) : jsonData,
         table = document.querySelector('.table'),
@@ -53,7 +55,7 @@ function renderPagination(jsonData) {
   renderActivePage(data);
 }
 
-//функция отрисовывает страницу, выбранную в пагинации
+//функция, которая отрисовывает страницу, выбранную в пагинации
 function renderActivePage(jsonData) {
   const pagNums = document.querySelectorAll('.pagination-number');
   
@@ -65,7 +67,7 @@ function renderActivePage(jsonData) {
   })
 }
 
-//показывает/отмечает активную страницу на пагинации
+//функция, которая отмечает активную страницу
 function markActivePageInPagination(pagNum) {
   const pagNums = document.querySelectorAll('.pagination-number');
 
@@ -78,9 +80,10 @@ function markActivePageInPagination(pagNum) {
   })
 }
 
-//4 - прорисовка таблицы. Создает строку таблицы с ячейками данных и добавляет их в tbody.
-//При прорисовке данных в колонке "Описание" обрезает about до длины th "Описание" (aboutLength ) деленное на 5.
-//aboutThLength / 5 примерно равно кол-ву символов шрифта Arial 16px, которые влезут 2-мя строками в ячейку about
+// функция, осуществляющая отрисовку таблицы: создает строку таблицы с ячейками данных и добавляет их в tbody
+/*При прорисовке данных в колонке "Описание" обрезаем about до длины th "Описание" (aboutLength), деленной на 5, т.к
+aboutThLength / 5 примерно равно кол-ву символов шрифта Arial 16px*/
+
 function renderCell(jsonData, pagNum = 1) {
   localStorage.getItem('jsonData') ? '' : localStorage.setItem('jsonData', JSON.stringify(jsonData));
   
@@ -109,14 +112,14 @@ function renderCell(jsonData, pagNum = 1) {
   });
 }
 
-//Сортировка.
-//навешивает событие на ячейки-заголовки таблицы
+// функция, выполняющая сортировку данных в таблице 
+// вешаем событие "клика" на ячейки-заголовки таблицы
 function eventSortTable() {
   const tableThs = document.querySelectorAll('th');
 
   tableThs.forEach((th, i) => {
     th.addEventListener('click', () => {
-      checkSelectedTh(i); // убирает класс selected и data-атрибут у неактивных заголовочных ячеек таблицы
+      checkSelectedTh(i); // данная функция (описана ниже) убирает класс selected и data-атрибут у неактивных заголовочных ячеек таблицы
 
       if (!th.dataset.order || th.dataset.order === '-1') {
         th.setAttribute('data-order', 1);
@@ -145,7 +148,7 @@ function checkSelectedTh(index) {
 }
 
 //функция sortTable() принимает индекс колонки которую нужно отсортировать и order, который используется
-//для сортировки по возрастанию и убыванию. order = 1 || order = -1
+// order = 1 || order = -1 по возрастанию || по убыванию
 function sortTable(index, order) {
   const tableRows = document.querySelectorAll('.data-row'),
         tableData = document.querySelector('#myTable');
@@ -158,14 +161,14 @@ function sortTable(index, order) {
 }
 
 
-// 9 - перерисовываем таблицу при изменении размера окна
+//  перерисовываем таблицу при изменении размера окна
 window.addEventListener('resize', () => {
   getData().then(() => {
     renderCell( JSON.parse( localStorage.getItem('jsonData') ) );
   });
 });
 
-//Сначала выполнится функция получения данных, затем все остальные
+// первой функция получения данных из JSON, затем все остальные функции
 getData().then((jsonData) => {
   renderCell(jsonData);
  renderPagination(jsonData);
